592bd5c99dc815a553ed05024745ebdd02da33ba
ChauVinhTienIT <vinhtienchau.it@gmail.com>
2026-04-17 22:23:15 +0700

    Show app loading spinner until UI ready
    Add a simple HTML/CSS loading overlay in index.html and show the UI5 BusyIndicator immediately during component init. Inject sap/ui/core/BusyIndicator, call BusyIndicator.show(0) on init and hide it once the first route is matched; also remove the standalone HTML loader element (#drs-app-loading) if present. This ensures a visible loading state until the app's first page is fully loaded.

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,11 +1,32 @@
 sap.ui.define(
-    ["sap/fe/core/AppComponent"],
-    function (Component) {
+    ["sap/fe/core/AppComponent", "sap/ui/core/BusyIndicator"],
+    function (Component, BusyIndicator) {
         "use strict";
 
         return Component.extend("z.sap01.cfa.Component", {
             metadata: {
                 manifest: "json"
+            },
+
+            init: function () {
+                // Show busy indicator immediately (delay=0)
+                BusyIndicator.show(0);
+
+                Component.prototype.init.apply(this, arguments);
+
+                // Hide when the first route is matched (page fully loaded)
+                var oRouter = this.getRouter();
+                if (oRouter) {
+                    oRouter.attachEventOnce("routeMatched", function () {
+                        BusyIndicator.hide();
+                    });
+                }
+
+                // Also hide the HTML spinner if running standalone (index.html)
+                var oLoader = document.getElementById("drs-app-loading");
+                if (oLoader) {
+                    oLoader.remove();
+                }
             }
         });
     }
